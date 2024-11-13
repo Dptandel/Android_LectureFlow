bf3fc465a81becfd7253146395e217cdd10bcb4f
Dharmin Tandel <dharmin28052002@gmail.com>
2024-11-13 08:07:17 +0530

    File Updated

---
+++ src/Android/Database/Pr22.kt
@@ -0,0 +1,40 @@
+package Android.Database
+
+// DAY 63
+
+// Learn about SQLiteDatabase and Room Database in brief.
+
+// DAY 64
+
+// Self Learn : Room Database (from YouTube) from Cheezy Code
+
+// DAY 65
+
+// Started ROOM DataBase with usage, benefits, etc.
+// TASK : RoomDB with RecyclerView of User Contact Details.
+
+// DAY 66
+
+// Room Database till Inserting data into Database and see "app inspection" of android studio -> to see data in Database.
+
+// DAY 67
+
+// Room Database data refresh through Request Code, BackPressed and Finish with adding data or only resume app
+
+// DAY 68
+
+// Room Database: Delete data on LongClick of item
+// Room Database: Update data on Click of item       ///SelfLearn : (DiffUtil) with MVVM
+// Complete Room Database
+
+// DAY 69
+
+// Started Network Connection with API from HTTP Methods (in Postman App)
+
+// DAY 70
+
+// Started JSON (JavaScript Object Notation) : what is JSON?
+
+// DAY 71
+
+//
--- src/Android/UIDesigning/Pr21.kt
+++ src/Android/UIDesigning/Pr21.kt
@@ -183,34 +183,3 @@ package Android.UIDesigning
 // DAY 62
 
 // Practice till learned concepts and topics at home( sir on leave )
-
-// DAY 63
-
-// Learn about SQLiteDatabase and Room Database in brief.
-
-// DAY 64
-
-// Self Learn : Room Database (from YouTube) from Cheezy Code
-
-// DAY 65
-
-// Started ROOM DataBase with usage, benefits, etc.
-// TASK : RoomDB with RecyclerView of User Contact Details.
-
-// DAY 66
-
-// Room Database till Inserting data into Database and see "app inspection" of android studio -> to see data in Database.
-
-// DAY 67
-
-// Room Database data refresh through Request Code, BackPressed and Finish with adding data or only resume app
-
-// DAY 68
-
-// Room Database: Delete data on LongClick of item
-// Room Database: Update data on Click of item       ///SelfLearn : (DiffUtil) with MVVM
-// Complete Room Database
-
-// DAY 69
-
-// Started Network Connection with API from HTTP Methods (in Postman App)
