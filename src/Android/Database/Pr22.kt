521ec68f21e8626771ebfcb05dbd0a09280e6429
Dharmin Tandel <dharmin28052002@gmail.com>
2024-11-14 09:22:14 +0530

    File Updated

--- src/Android/Database/Pr22.kt
+++ src/Android/Database/Pr22.kt
@@ -1,5 +1,37 @@
 package Android.Database
 
+// DAY 56
+
+// Learn Android Storage Options : Online and Offline.
+// Offline : 1. SharedPreference 2. SQLite Database 3. Internal or External File Storage
+// Online : 1. Network Connection
+// Started SharedPreference ( it stores data as key/value pair )
+
+// DAY 57
+
+// TASK : using SharedPreferences manage session management ( like, splash screen to onBoarding (1 time only)
+// to Login (if you have registered) / to register (if not registered) do this task
+
+// DAY 58
+
+// Work on Previous Task of SharedPreferences.
+
+// DAY 59
+
+// Searching for figma UI Design to implement as Projects. (selfLearn)
+
+// DAY 60
+
+// SharedPreferences with Singleton Pattern ( for single object creation not multiple time creation )
+
+// DAY 61
+
+// Work on FoodNinja App from Figma Design.
+
+// DAY 62
+
+// Practice till learned concepts and topics at home( sir on leave )
+
 // DAY 63
 
 // Learn about SQLiteDatabase and Room Database in brief.
--- src/Android/UIDesigning/Pr21.kt
+++ src/Android/UIDesigning/Pr21.kt
@@ -151,35 +151,3 @@ package Android.UIDesigning
 // 3. Nested DropDown in Android (Like, one EditText and one Button On screen, if we enter one category like, fruit than
 //    it will add into spinner DropDown then if we select that fruits category in spinner then if we enter the apple fruit
 //    then it will add into that fruit category in like nested DropDown that's it
-
-// DAY 56
-
-// Learn Android Storage Options : Online and Offline.
-// Offline : 1. SharedPreference 2. SQLite Database 3. Internal or External File Storage
-// Online : 1. Network Connection
-// Started SharedPreference ( it stores data as key/value pair )
-
-// DAY 57
-
-// TASK : using SharedPreferences manage session management ( like, splash screen to onBoarding (1 time only)
-// to Login (if you have registered) / to register (if not registered) do this task
-
-// DAY 58
-
-// Work on Previous Task of SharedPreferences.
-
-// DAY 59
-
-// Searching for figma UI Design to implement as Projects. (selfLearn)
-
-// DAY 60
-
-// SharedPreferences with Singleton Pattern ( for single object creation not multiple time creation )
-
-// DAY 61
-
-// Work on FoodNinja App from Figma Design.
-
-// DAY 62
-
-// Practice till learned concepts and topics at home( sir on leave )
