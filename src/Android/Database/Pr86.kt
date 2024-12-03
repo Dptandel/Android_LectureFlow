6e6cc76c8196a794139bf9cd4cba0bf7b2d8532e
Dharmin Tandel <dharmin28052002@gmail.com>
2024-12-03 10:27:17 +0530

    File Uploaded

--- src/Android/Database/Pr86.kt
+++ src/Android/Database/Pr86.kt
@@ -3,3 +3,11 @@ package Android.Database
 // DAY 86
 
 // QR Code Scanner App by YouTube (Android Knowledge) (using ML Kit and CameraX of JetPack)
+
+// ( New Faculty - Jay Prajapati Sir)
+// DAY 87
+
+// Revised Kotlin and Android till Database
+// new things for R&D
+// 1. Android Version name with unique features in it from previous versions to new one version
+// 2. Types of Fragment
